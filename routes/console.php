ae978de6de3acbc3fe5d312572f6a47085f79e3a
Your Name <youremail@domain.com>
2022-05-21 14:18:45 +0900

    dfiuzui44aeiuiu

--- routes/console.php
+++ routes/console.php
@@ -3,17 +3,10 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-/*
-|--------------------------------------------------------------------------
-| Console Routes
-|--------------------------------------------------------------------------
-|
-| This file is where you may define all of your Closure based console
-| commands. Each Closure is bound to a command instance allowing a
-| simple approach to interacting with each command's IO methods.
-|
-*/
+$fFunction = function () {
+  $quote = Inspiring::quote();
+  $this->comment($quote);
+};
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+$command = Artisan::command('inspire', $fFunction);
+$command->purpose('Display an inspiring quote');
--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+$fFunction = function () {return view('welcome');};
+Route::get('/', $fFunction);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+$arr = [
+  'auth:sanctum',
+  config('jetstream.auth_session'),
+  'verified'
+];
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+$middleware = Route::middleware($arr);
+
+$fFunction = function () {
+  $fFunction = function () {return view('dashboard');};
+  $get = Route::get('/dashboard', $fFunction);
+  $get->name('dashboard');
+};
+
+$middleware->group($fFunction);
